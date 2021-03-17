46a432e83d4330d8a6aefa28e0e40c4ed98116e5
Utkarsh Goel <ugoel911@gmail.com>
2021-03-17 10:51:51 +0530

    teacher login route and db connected

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("./db/db");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const studentRouter = require("./routes/students");
+const teacherRouter = require("./routes/teachers");
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/students", studentRouter);
+app.use("/teachers", teacherRouter);
 
 module.exports = app;
--- routes/teachers.js
+++ routes/teachers.js
@@ -4,13 +4,12 @@ const route = express.Router();
 
 route.post("/login", (req, res) => {
     const {email, name} = req.body;
-    db.query("SELECT * FROM students WHERE email = ?", [email], (err, results, fields) => {
+    db.query("SELECT * FROM teachers WHERE email = ?", [email], (err, results, fields) => {
         if (err) {
             throw new Error(err);
         }
         if (results.length === 0) {
-            console.log(name.substr(2, 10), name.substr(11), email);
-            db.query("INSERT INTO students (sid, name, email) VALUES (?, ?, ?)", [parseInt(name.substr(2, 10)), name.substr(11), email], (err, results, fields) => {
+            db.query("INSERT INTO teachers (name, email) VALUES (?, ?)", [name, email], (err, results, fields) => {
                 if (err) {
                     throw new Error(err);
                 } else {
