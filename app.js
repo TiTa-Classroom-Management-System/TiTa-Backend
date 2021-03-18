33bde5a8553043b9b13762e1e5fd0163d87abaee
solarconstant <ashishshubh001@gmail.com>
2021-03-18 10:53:59 +0530

    Add dynamic routing

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/students", studentRouter);
-app.use("/teachers", teacherRouter);
-app.use("/classrooms", classroomRouter);
+
+//routes dynamically
+fs.readdirSync('./routes').map((route) => app.use("/api", require(`./routes/${route}`)))
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+// app.use("/students", studentRouter);
+// app.use("/teachers", teacherRouter);
+// app.use("/classrooms", classroomRouter);
 
 module.exports = app;
