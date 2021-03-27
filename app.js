751802d7e6d1beb2448dc75bea8481f0b800f67a
solarconstant <ashishshubh001@gmail.com>
2021-03-28 00:02:44 +0530

    Add Dynamic route creation

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-<<<<<<< HEAD
 const fs = require("fs");
-=======
->>>>>>> 2ed19baed75f3304389aa4631d0641270a024ab8
 require("dotenv").config();
 require("./db/db");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const studentRouter = require("./routes/students");
-const teacherRouter = require("./routes/teachers");
-const classroomRouter = require("./routes/classroom");
 
 const app = express();
 
@@ -26,14 +17,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-
-//routes dynamically
-fs.readdirSync('./routes').map((route) => app.use("/api", require(`./routes/${route}`)))
-
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-// app.use("/students", studentRouter);
-// app.use("/teachers", teacherRouter);
-// app.use("/classrooms", classroomRouter);
+//dynamic route creation
+fs.readdirSync("./routes").map((route) => app.use(require(`./routes/${route}`)));
 
 module.exports = app;
--- routes/students.js
+++ routes/students.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const db = require('../db/db');
-const router = express.Router();
-
-router.post("/login", (req, res) => {
-    const {email, name} = req.body;
-    db.query("SELECT * FROM students WHERE email = ?", [email], (err, results, fields) => {
-        if (err) {
-            throw new Error(err);
-        }
-        if (results.length === 0) {
-            console.log(name.substr(2, 10), name.substr(11), email);
-            db.query("INSERT INTO students (sid, name, email) VALUES (?, ?, ?)", [parseInt(name.substr(2, 10)), name.substr(11), email], (err, results, fields) => {
-                if (err) {
-                    throw new Error(err);
-                } else {
-                    res.status(200).send();
-                }
-            });
-        }
-        res.status(200).send();
-    });
-});
-
-module.exports = router;
-=======
 const express = require("express");
 const db = require("../db/db");
 const route = express.Router();
@@ -58,4 +31,3 @@ route.post("/login", (req, res) => {
 });
 
 module.exports = route;
->>>>>>> 2ed19baed75f3304389aa4631d0641270a024ab8
