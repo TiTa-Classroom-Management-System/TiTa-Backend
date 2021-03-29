d1dedb1402bc2d1c54576cb28da1aa7505e9fc46
solarconstant <ashishshubh001@gmail.com>
2021-03-29 13:39:51 +0530

    Make classroom_id VARCHAR

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 require("./db/db");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const studentRouter = require("./routes/students");
 const teacherRouter = require("./routes/teachers");
 const classroomRouter = require("./routes/classroom");
@@ -22,7 +21,6 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/students", studentRouter);
 app.use("/teachers", teacherRouter);
 app.use("/classroom", classroomRouter);
--- routes/classroom.js
+++ routes/classroom.js
@@ -5,9 +5,9 @@ const db = require("../db/db");
 router.post("/create", async (req, res) =>
 {
     const {subjectName, subjectCode, subGroups} = req.body;
-    let classroom_id = 1;
+    let classroom_id = "qwertyuiop";
     await db.query("SELECT * FROM classrooms where classroom_id = ?", 
-                [parseInt(classroom_id)], (err, results, fields) =>
+                [classroom_id], (err, results, fields) =>
                 {
                     if(err)
                     {
@@ -15,21 +15,27 @@ router.post("/create", async (req, res) =>
                     }
                     if(results.length === 0)
                     {
-                        db.query("INSERT INTO classrooms VALUES (?, ?, ?, ?)", 
-                                    [classroom_id, subjectName, subjectCode, subGroups],
-                                    (err, results, fields) =>
+                        try
+                        {
+                                db.query("INSERT INTO classrooms VALUES (?, ?, ?, ?)", 
+                                [classroom_id, subjectName, subjectCode, subGroups],
+                                (err, results, fields) =>
+                                {
+                                    if(err)
                                     {
-                                        if(err)
-                                        {
-                                            throw new Error(err);
-                                            return;
-                                        }
-                                        else
-                                        {
-                                            res.status(200).send("Created Classroom.");
-                                            return;
-                                        }
-                                    })
+                                        throw new Error(err);
+                                    }
+                                    else
+                                    {
+                                        res.status(200).send("Created Classroom.");
+                                        return;
+                                    }
+                                });
+                        }
+                        catch (err)
+                        {
+                            throw new Error(err);
+                        }
                     }
                 });
 });
