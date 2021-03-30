f29767adf82810999c353a40ed9342898a42c410
Utkarsh Goel <ugoel911@gmail.com>
2021-03-30 20:32:46 +0530

    student controllers added

--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const db = require("../db/db");
 const router = express.Router();
+const { login, getTimeTable } = require("../controllers/students");
 
-router.post("/login");
+router.post("/login", login);
 
-router.get("/time_table/:email");
+router.get("/time_table/:email", getTimeTable);
 
 module.exports = router;
