d1c004869a1153d056377115f262ee79ebf6fa70
Utkarsh Goel <ugoel911@gmail.com>
2021-03-30 20:30:16 +0530

    controllers seperated from routes

--- routes/classroom.js
+++ routes/classroom.js
@@ -1,57 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db/db");
+const { createClassRoom } = require("../controllers/classroom");
 
-router.post("/create", async (req, res) =>
-{
-    const {subjectName, subjectCode, subGroups} = req.body;
-    let classroom_id = "asdfghjkl";
-    await db.query("SELECT * FROM classrooms where classroom_id = ?", 
-                [classroom_id], (err, results, fields) =>
-                {
-                    if(err)
-                    {
-                        throw new Error(err);
-                    }
-                    if(results.length === 0)
-                    {
-                        try
-                        {
-                                db.query("INSERT INTO classrooms VALUES (?, ?, ?, ?)", 
-                                [classroom_id, subjectName, subjectCode, subGroups],
-                                (err, results, fields) =>
-                                {
-                                    if(err)
-                                    {
-                                        throw new Error(err);
-                                    }
-                                    else
-                                    {
-                                        res.status(200).send("Created Classroom.");
-                                    }
-                                });
-                                for(let i=1;i<=subGroups;i++){
-                                    db.query("INSERT INTO sub_class(class_id,grp_no) VALUES (?,?)",
-                                    [classroom_id,parseInt(i)],
-                                    (err,results,field)=>
-                                    {
-                                        if(err)
-                                        {
-                                            throw new Error(err);
-                                        }
-                                        // else
-                                        // {
-                                        //     res.status(200).send("Created SubClassroom.");
-                                        // }
-                                    })
-                                }                                
-                        }
-                        catch (err)
-                        {
-                            throw new Error(err);
-                        }
-                    }
-                });
-});
+router.post("/create", createClassRoom);
 
 module.exports = router;
