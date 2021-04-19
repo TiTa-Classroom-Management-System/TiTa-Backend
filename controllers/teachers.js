51b90536ee374e6cdd8f30e0a423d5fa5ea2b3f7
Taranjot Singh <taranjotsfeb2001@gmail.com>
2021-04-19 20:58:10 +0530

    Send Group Numbers with Assignment Data

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -140,7 +140,7 @@ const getAssignment = (req, res) => {
   const classid = req.params.classid;
   console.log(classid)
   db.query(
-    "SELECT assignment_name, creation_date, submission_date, assignment_link FROM assignment WHERE assignment_id IN (SELECT assignment_id FROM assignment_subclass WHERE sub_class_id IN (SELECT sub_class_id FROM sub_class WHERE class_id = ? ))",
+    "SELECT assignment_name, creation_date, submission_date, assignment_link, grp_no FROM (SELECT * FROM assignment WHERE assignment_id IN (SELECT assignment_id FROM assignment_subclass WHERE sub_class_id IN (SELECT sub_class_id FROM sub_class WHERE class_id = ? ))) A JOIN (SELECT assignment_id, grp_no FROM assignment_subclass X JOIN sub_class Y ON X.sub_class_id=Y.sub_class_id) B ON A.assignment_id=B.assignment_id",
     [classid],
     (err, results, fields) => {
       if(err) throw new Error(err);
